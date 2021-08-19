fb69240c4217cd6fae5c25bc1f029fa194829dd8
Jorge Enrique Vargas Cifuentes <jorge.vargas@vasslatam.com>
2021-08-19 10:41:41 -0500

    add MSRegistry

---
+++ MSRegistry/src/test/java/co/com/store/registry/MsRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
